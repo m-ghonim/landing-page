5d2ba0bb9d4c8d979ad2f57328907dafd7f88aa1
m-ghonim <mohamedghoneam95@gmail.com>
2021-02-19 12:21:03 +0200

    feat: Add nav link highlight for active section

--- README.md
+++ README.md
@@ -2,12 +2,50 @@
 
 ## Table of Contents
 
-* [Instructions](#instructions)
+* [Features](#Features)
 
-## Instructions
+## Features
+Based on [starter files](https://github.com/udacity/fend/tree/refresh-2019/projects/landing-page) from Udacity FEND repo.
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+This projects is a a multi-section landing page, with a dynamically constructed navigation menu based on the amount of content that is added to the page.
+### Dynamic Navigation Menu
+- A navigation menu entry is created for each section in the page with a `data-nav` attribute
+- The link text is based on the attribute's value
+- Clicking on the link will scroll the related ssection into the view port with a smooth scrolling effect
+### Active Section and Navigation Link Highlight
+- The section closest to the top edge of the view port is highlighted using the css class `your-active-class`
+- If two or more sections are in the view port, the one with its top edge directly _below_ viewport's top edge is highlited
+- If a section is taking the entirety of the viewport, it will remain highlited untill the next section comes into view
+- The section's navigation link is also highlighted using the css class `active-nav-link`
 
-To get started, open `js/app.js` and start building out the app's functionality
+## Function Documentation
+### buildNavMenu()
+- Can be called at run time to construct/reconstruct the navigation menu by creating a nav menu entry for each `section` in the page with `data-nav` attribute
+- Each entry is constructed as
+```
+<li>
+    <a href="#sectionID" class="menu__link" data-navlink="sectionID">
+	    data-nav-value
+    </a>
+</li>
+```
+    *sectionID: ID of linked section
+	*data-nav-value: Text value of the section's data-nav attribute
+- The navlink data attribute is used to highlight the link when its section is inside viewport
+- After the menu is constructed, it's appended to the element with `id="navbar__list"` which should be a `ul` element
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+### highlightActiveSection()
+- Highlights the section closest to the top edge of the view port by adding the css class `your-active-class` to it and removing it from other sections
+
+### scrollToAnchorID(a)
+- Can be added to the click event of any anchor linked to an element ID to smoothly scroll vertically to the element upon clicking the anchor
+- Tales event object parameter {a} passed from the clicked <a> element
+- Uses the `Window.scroll()` method to guarantee compatibility with older browsers that don't support `Element.scrollIntoView()` smooth behavior
+
+## Version History
+- Initial commit
+- Add build navigation menu function
+- Add highlight active section
+- fix: Highlight active section if it's larger than the viewport
+- Add smooth scroll to nav links
+- fix: Highlight active section if its top edge matches viewport top
--- js/app.js
+++ js/app.js
@@ -32,7 +32,9 @@ let scrolling = true;
  */
 
 // build the nav
-// creates a nav menu entry for each section with data-nav attribute, link text is set to the attribute's value
+/**
+ * @description creates a nav menu entry for each section with data-nav attribute, link text is set to the attribute's value
+ */
 function buildNavMenu() {
   const sections = document.querySelectorAll("section[data-nav]");
   let navMenu = document.createDocumentFragment();
@@ -40,6 +42,7 @@ function buildNavMenu() {
     let li = document.createElement("li");
     let a = document.createElement("a");
     a.href = `#${section.id}`;
+    a.dataset.navlink = section.id;  // navlink data attribute used highlight the link when its section is inside viewport
     a.textContent = section.dataset.nav;
     a.classList.add("menu__link");
     li.appendChild(a);
@@ -56,7 +59,7 @@ function highlightActiveSection() {
   let negClosestTop = -10 * window.innerHeight; // tracks the closest negative DomRect.top to viewport, in case no positive ones are found
   let active = -1; // stores the index of the active section
   sections.forEach((section, i) => {
-    section.classList.remove('your-active-class')
+    section.classList.remove("your-active-class");
     const rectTop = section.getBoundingClientRect().top;
     if (rectTop >= 0 && rectTop < closestTop) {
       closestTop = rectTop;
@@ -64,14 +67,21 @@ function highlightActiveSection() {
     }
     // check in case active section is larger than the view port (top < 0)
     else if (rectTop < 0 && rectTop > negClosestTop && active <= 0) {
-      negClosestTop = rectTop
-      active = i * -1
+      negClosestTop = rectTop;
+      active = i * -1;
     }
-  })
-  if (active >= 0) {
-    sections[active].classList.add('your-active-class');
-  } else {
-    sections[active * -1].classList.add('your-active-class');
+  });
+  active = active < 0 ? active * -1 : active;
+  sections[active].classList.add("your-active-class");
+
+  // highlight nav link when its section is active (inside view port)
+  const links = document.querySelectorAll('a[data-navlink]');
+  for (link of links) {
+    if (link.dataset.navlink == sections[active].id) {
+      link.classList.add("active-nav-link");
+      continue;
+    }
+    link.classList.remove("active-nav-link")
   }
 }
 
@@ -84,11 +94,13 @@ function highlightActiveSection() {
 // https://caniuse.com/?search=scrollintovie
 function scrollToAnchorID(a) {
   a.preventDefault();
-  const anchorEleemnt = document.getElementById(a.target.getAttribute("href").slice(1));
+  const anchorEleemnt = document.getElementById(
+    a.target.getAttribute("href").slice(1)
+  );
   const y = anchorEleemnt.getBoundingClientRect().top + window.scrollY;
   window.scroll({
     top: y,
-    behavior: 'smooth'
+    behavior: "smooth",
   });
 }
 /**
@@ -104,19 +116,18 @@ window.addEventListener("DOMContentLoaded", () => {
   // Add smooth scroll event to navigation menu
   let navLinks = document.querySelectorAll('nav li a[href^="#"]');
   for (navLink of navLinks) {
-    navLink.addEventListener('click', scrollToAnchorID);
+    navLink.addEventListener("click", scrollToAnchorID);
   }
 });
 
-
 // Set sections as active when scrolling
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   scrolling = true;
-})
+});
 // ensure the highlight function is fired async. and not repeatedly
-setInterval( () => {
+setInterval(() => {
   if (scrolling) {
     scrolling = false;
     highlightActiveSection();
   }
-}, 50 );
+}, 50);
